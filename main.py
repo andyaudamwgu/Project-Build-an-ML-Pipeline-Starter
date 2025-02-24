607f9b23c03430a9bcaad99b7278a431fc927c5e
Mien Au <mienau@Miens-MBP.attlocal.net>
2025-02-24 00:00:24 -0600

    Add data_check step with passing tests

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-
 import mlflow
 import tempfile
 import os
@@ -13,30 +12,20 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
 ]
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
-    # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
-    # Steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
-    # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
-
         if "download" in active_steps:
-            # Download file and load in W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
@@ -51,47 +40,66 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            basic_cleaning_run_id = _run_basic_cleaning(
+                input_artifact="adam6-western-governors-university/nyc_airbnb/sample.csv:latest",
+                output_artifact="clean_sample.csv",
+                output_type="clean_data",
+                output_description="Cleaned NYC Airbnb data",
+                min_price=config["etl"]["min_price"],
+                max_price=config["etl"]["max_price"]
+            )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _run_data_check(
+                csv="adam6-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",
+                ref="adam6-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:reference",
+                kl_threshold=config["data_check"]["kl_threshold"],
+                min_price=config["etl"]["min_price"],
+                max_price=config["etl"]["max_price"]
+            )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
             pass
 
         if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
             pass
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
             pass
 
+def _run_basic_cleaning(input_artifact, output_artifact, output_type, output_description, min_price, max_price):
+    run_id = mlflow.run(
+        os.path.join(ROOT_DIR, "src", "basic_cleaning"),
+        "main",
+        env_manager="conda",
+        parameters={
+            "input_artifact": input_artifact,
+            "output_artifact": output_artifact,
+            "output_type": output_type,
+            "output_description": output_description,
+            "min_price": min_price,
+            "max_price": max_price
+        }
+    )
+    return run_id
+
+def _run_data_check(csv, ref, kl_threshold, min_price, max_price):
+    run_id = mlflow.run(
+        os.path.join(ROOT_DIR, "src", "data_check"),
+        "main",
+        env_manager="conda",
+        parameters={
+            "csv": csv,
+            "ref": ref,
+            "kl_threshold": kl_threshold,
+            "min_price": min_price,
+            "max_price": max_price
+        }
+    )
+    return run_id
 
 if __name__ == "__main__":
     go()
