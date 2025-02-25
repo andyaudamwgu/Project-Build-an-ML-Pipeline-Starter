3cc099edf3acff65746292a14847b6b012c5b947
Mien Au <mienau@Miens-MBP.attlocal.net>
2025-02-24 23:44:22 -0600

    Complete Step 5: Updated main.py to use local test_run.py with subprocess.run for W&B logging

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-
 import mlflow
 import tempfile
 import os
 import wandb
 import hydra
+import subprocess
 from omegaconf import DictConfig
 
 _steps = [
@@ -13,30 +13,21 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    # "test_regression_model"  # Uncomment if you want it in the default pipeline
 ]
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
-
-    # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
-    # Steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
-    # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
-
         if "download" in active_steps:
-            # Download file and load in W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
@@ -51,47 +42,107 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            basic_cleaning_run_id = _run_basic_cleaning(
+                input_artifact="adam6-western-governors-university/nyc_airbnb/sample.csv:latest",
+                output_artifact="clean_sample.csv",
+                output_type="clean_data",
+                output_description="Cleaned NYC Airbnb data",
+                min_price=config["etl"]["min_price"],
+                max_price=config["etl"]["max_price"]
+            )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _run_data_check(
+                csv="adam6-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",
+                ref="adam6-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:reference",
+                kl_threshold=config["data_check"]["kl_threshold"],
+                min_price=config["etl"]["min_price"],
+                max_price=config["etl"]["max_price"]
+            )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input": "adam6-western-governors-university/Project-Build-an-ML-Pipeline-Starter-src_basic_cleaning/clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                }
+            )
 
         if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
+            _run_train_random_forest(
+                trainval_artifact="adam6-western-governors-university/nyc_airbnb/trainval_data.csv:latest",
+                val_size=config["modeling"]["val_size"],
+                random_seed=config["modeling"]["random_seed"],
+                stratify_by=config["modeling"]["stratify_by"],
+                output_artifact="random_forest_export",
+                rf_config=rf_config,
+                max_tfidf_features=config["modeling"]["max_tfidf_features"]
+            )
 
         if "test_regression_model" in active_steps:
+            script_path = os.path.join(ROOT_DIR, "test_run.py")
+            cmd = [
+                "python", script_path,
+                "--mlflow_model", "adam6-western-governors-university/nyc_airbnb/random_forest_export:prod",
+                "--test_dataset", "adam6-western-governors-university/nyc_airbnb/test_data.csv:latest"
+            ]
+            subprocess.run(cmd, check=True)
 
-            ##################
-            # Implement here #
-            ##################
+def _run_basic_cleaning(input_artifact, output_artifact, output_type, output_description, min_price, max_price):
+    run_id = mlflow.run(
+        os.path.join(ROOT_DIR, "src", "basic_cleaning"),
+        "main",
+        env_manager="conda",
+        parameters={
+            "input_artifact": input_artifact,
+            "output_artifact": output_artifact,
+            "output_type": output_type,
+            "output_description": output_description,
+            "min_price": min_price,
+            "max_price": max_price
+        }
+    )
+    return run_id
 
-            pass
+def _run_data_check(csv, ref, kl_threshold, min_price, max_price):
+    run_id = mlflow.run(
+        os.path.join(ROOT_DIR, "src", "data_check"),
+        "main",
+        env_manager="conda",
+        parameters={
+            "csv": csv,
+            "ref": ref,
+            "kl_threshold": kl_threshold,
+            "min_price": min_price,
+            "max_price": max_price
+        }
+    )
+    return run_id
 
+def _run_train_random_forest(trainval_artifact, val_size, random_seed, stratify_by, output_artifact, rf_config, max_tfidf_features):
+    run_id = mlflow.run(
+        os.path.join(ROOT_DIR, "src", "train_random_forest"),
+        "main",
+        env_manager="conda",
+        parameters={
+            "trainval_artifact": trainval_artifact,
+            "val_size": val_size,
+            "random_seed": random_seed,
+            "stratify_by": stratify_by,
+            "output_artifact": output_artifact,
+            "rf_config": rf_config,
+            "max_tfidf_features": max_tfidf_features
+        }
+    )
+    return run_id
 
 if __name__ == "__main__":
     go()
